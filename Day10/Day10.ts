9c584f4441ce3aa877b3ec8afbcbdd98adf0d75f
mmozzo96 <micky.mozzo@gmial.com>
2022-12-10 12:32:33 +0200

    Day 10 part 2 solution

--- Day10/Day10.ts
+++ Day10/Day10.ts
@@ -60,5 +60,31 @@ const {solution: solutionPart1, cycles} = solution1(Data, InterestingCycles)
 
 console.log(solutionPart1)
 
+function drawPixels(register: number, pixel: number): ('#'|' ') {
+  if(Math.abs(register-pixel) < 2) return '#'
+  return ' '
+}
+
+const lines = 6
+const pixelsPerLine: number = 40
+
+function solution2(cycles: number[]): string {
+  let solution: string = ''
+
+  for(let lineIdx=0;lineIdx<lines;lineIdx++) {
+    let line: string = ''
+    for(let pixel=0;pixel<pixelsPerLine;pixel++) {
+      const cycle = (pixel + lineIdx*40)
+      line += drawPixels(cycles[cycle], pixel)
+    }
+    solution += line+'\r\n' // \r is needed for windows OS otherwise \n alone should be used
+  }
+
+  return solution.replace(/(.{1})/g,"$1 ") // this is added for readibility
+}
+
+console.log(solution2(cycles))
+
+
 
 
