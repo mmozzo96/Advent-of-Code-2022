7c0900629855fee50c8435e1f21ddbebb72755c5
mmozzo96 <micky.mozzo@gmial.com>
2022-12-11 15:18:22 +0200

    Day 11 part 1 solution

--- Day11/Day11.ts
+++ Day11/Day11.ts
@@ -1,15 +1,15 @@
 import * as fs from "fs";
 import path from "path";
-var cloneDeep = require('lodash.clonedeep');
+var cloneDeep = require("lodash.clonedeep");
 
 const Data: string[][] = fs
   .readFileSync(path.join(__dirname, "Day11Data.txt"), "utf8")
   .split(/(\r?\n)+(\r?\n)/)
   .filter((chuncks: string, idx: number) => idx % 3 === 0)
-  .map(monkey=>{
-    const monkeyLines: string[] = monkey.split(/\r?\n/)
-    return monkeyLines.slice(1)
-  })
+  .map((monkey) => {
+    const monkeyLines: string[] = monkey.split(/\r?\n/);
+    return monkeyLines.slice(1);
+  });
 
 interface operation {
   (item: number): number;
@@ -18,7 +18,7 @@ interface test {
   (item: number): boolean;
 }
 interface toMonkey {
-  (testResult: boolean): number
+  (testResult: boolean): number;
 }
 
 interface monkey {
@@ -83,72 +83,132 @@ function readStartingMonkey(description: string[]): monkey {
     )
   )
     throw new Error();
-  const isTrue: number = +description[3].slice(fourthRow.length)
-  const isFalse: number = +description[4].slice(fifthRow.length)
-  const toMonkey = (testResult: boolean) => testResult ? isTrue : isFalse
-  
+  const isTrue: number = +description[3].slice(fourthRow.length);
+  const isFalse: number = +description[4].slice(fifthRow.length);
+  const toMonkey = (testResult: boolean) => (testResult ? isTrue : isFalse);
+
   const startingMonkey: monkey = {
     items: stratingItems,
     operation,
     test,
-    toMonkey
-  }
+    toMonkey,
+  };
 
-  return startingMonkey
+  return startingMonkey;
 }
 
-const startingMonkeys: monkey[] = Data.map(description => readStartingMonkey(description))
+const startingMonkeys: monkey[] = Data.map((description) =>
+  readStartingMonkey(description)
+);
 
 function solution1(startingMonkeys: monkey[]) {
-  let numberOfInspections: number[] = new Array(startingMonkeys.length).fill(0)
+  let numberOfInspections: number[] = new Array(startingMonkeys.length).fill(0);
 
   async function monkeyTurn(monkeys: monkey[], idx: number) {
-    let {items, operation, test, toMonkey}: monkey = monkeys[idx]
-    const itemsLength: number = items.length
-    numberOfInspections[idx] += itemsLength
+    let { items, operation, test, toMonkey }: monkey = monkeys[idx];
+    const itemsLength: number = items.length;
+    numberOfInspections[idx] += itemsLength;
 
-    if(items.length<1) return
+    if (items.length < 1) return;
 
-    for (let i=0;i<itemsLength;i++) {
-      const item: number = <number>items.shift()
-      const itemAfterInspection: number = Math.floor(operation(item)/3)
-      const testResult: boolean = test(itemAfterInspection)
-      const itemIsMovedTo: number = toMonkey(testResult)
+    for (let i = 0; i < itemsLength; i++) {
+      const item: number = <number>items.shift();
+      const itemAfterInspection: number = Math.floor(operation(item) / 3);
+      const testResult: boolean = test(itemAfterInspection);
+      const itemIsMovedTo: number = toMonkey(testResult);
 
-      await monkeys[itemIsMovedTo].items.push(itemAfterInspection)
+      await monkeys[itemIsMovedTo].items.push(itemAfterInspection);
     }
   }
 
   async function doOneRound(monkeys: monkey[]): Promise<monkey[]> {
-    let copyMonkeys: monkey[] = cloneDeep(monkeys)
+    let copyMonkeys: monkey[] = cloneDeep(monkeys);
 
-    for(let i=0;i<copyMonkeys.length;i++) {
-      await monkeyTurn(copyMonkeys, i)
+    for (let i = 0; i < copyMonkeys.length; i++) {
+      await monkeyTurn(copyMonkeys, i);
     }
-    return copyMonkeys
+    return copyMonkeys;
   }
 
   async function doNRound(numberOfRounds: number, startingMonkeys: monkey[]) {
-    let finalMonkeys: monkey[] = cloneDeep(startingMonkeys)
-    for(let i=0;i<numberOfRounds;i++){
-      finalMonkeys = await doOneRound(finalMonkeys)
+    let finalMonkeys: monkey[] = cloneDeep(startingMonkeys);
+    for (let i = 0; i < numberOfRounds; i++) {
+      finalMonkeys = await doOneRound(finalMonkeys);
     }
   }
 
   async function logSolution() {
-    await doNRound(20, startingMonkeys)
-    const max1Idx = findIdxMax(numberOfInspections)
-    const max1 = numberOfInspections[max1Idx]
-    numberOfInspections.splice(max1Idx,1)
-    const max2 = Math.max(...numberOfInspections)
+    await doNRound(20, startingMonkeys);
+    const max1Idx = findIdxMax(numberOfInspections);
+    const max1 = numberOfInspections[max1Idx];
+    numberOfInspections.splice(max1Idx, 1);
+    const max2 = Math.max(...numberOfInspections);
+
+    console.log(max1 * max2);
+  }
+
+  logSolution();
+}
+
+solution1(startingMonkeys);
+
+const tests: number = new Array(Data.length).fill(1)
+  .map((one: number, idx: number) => {
+    return +Data[idx][2].slice('  Test: divisible by '.length)
+  }).reduce((sF: number, n: number) => sF*n)
+
+function solution2(startingMonkeys: monkey[], testMultiplied: number) {
+  let numberOfInspections: number[] = new Array(startingMonkeys.length).fill(0);
+
+  function newOperation(operation: operation, item: number): number {
+    if(item>testMultiplied) item = item%testMultiplied
+    return operation(item)
+  }
+
+  async function monkeyTurn(monkeys: monkey[], idx: number) {
+    let { items, operation, test, toMonkey }: monkey = monkeys[idx];
+    const itemsLength: number = items.length;
+    numberOfInspections[idx] += itemsLength;
+
+    if (items.length < 1) return;
+
+    for (let i = 0; i < itemsLength; i++) {
+      const item: number = <number>items.shift();
+      const itemAfterInspection: number = newOperation(operation, item);
+      const testResult: boolean = test(itemAfterInspection);
+      const itemIsMovedTo: number = toMonkey(testResult);
+
+      await monkeys[itemIsMovedTo].items.push(itemAfterInspection);
+    }
+  }
+
+  async function doOneRound(monkeys: monkey[]): Promise<monkey[]> {
+    let copyMonkeys: monkey[] = cloneDeep(monkeys);
+
+    for (let i = 0; i < copyMonkeys.length; i++) {
+      await monkeyTurn(copyMonkeys, i);
+    }
+    return copyMonkeys;
+  }
+
+  async function doNRound(numberOfRounds: number, startingMonkeys: monkey[]) {
+    let finalMonkeys: monkey[] = cloneDeep(startingMonkeys);
+    for (let i = 0; i < numberOfRounds; i++) {
+      finalMonkeys = await doOneRound(finalMonkeys);
+    }
+  }
+
+  async function logSolution() {
+    await doNRound(10000, startingMonkeys);
+    const max1Idx = findIdxMax(numberOfInspections);
+    const max1 = numberOfInspections[max1Idx];
+    numberOfInspections.splice(max1Idx, 1);
+    const max2 = Math.max(...numberOfInspections);
 
     console.log(max1 * max2)
   }
 
-  logSolution()
+  logSolution();
 }
 
-solution1(startingMonkeys)
-
-
-
+solution2(startingMonkeys, tests);
